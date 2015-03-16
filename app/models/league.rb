dbf0c205a36839b633841a33faf8c181960f3238
Lauris Bligzna <bligzna.lauris@gmail.com>
2015-03-16 09:37:24 +0200

    second commit

---
+++ app/models/league.rb
@@ -0,0 +1,4 @@
+class League 
+  include Neo4j::ActiveNode
+  
+end
---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails"
+
+%w(
+  neo4j
+  action_controller
+  action_mailer
+  sprockets
+).each do |framework|
+  begin
+    require "#{framework}/railtie"
+  rescue LoadError
+  end
+end
+
+Bundler.require(*Rails.groups)
+
+module FootballLeague
+ class Application < Rails::Application
+    config.generators do |g|
+      g.orm :neo4j
+    end
+
+    config.neo4j.session_options = { basic_auth: { username: 'neo4j', password: 'kalpaks12'} } 
+    config.neo4j.session_type = :server_db 
+    config.neo4j.session_path = 'http://localhost:7474'
+  end
+end
